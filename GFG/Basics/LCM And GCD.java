513b02ca14fefcdf648e3807533db8ae91347826
kashish1200 <84280963+kashish1200@users.noreply.github.com>
2025-04-17 00:37:59 +0530

    Update LCM And GCD.java

--- GFG/Basics/LCM And GCD.java
+++ GFG/Basics/LCM And GCD.java
@@ -16,3 +16,16 @@ class Solution {
             return ans;
     }
 };
+
+
+class Solution {
+    public static int gcd(int a, int b) {
+        int gcd=1;
+        for(int i=1;i<=Math.min(a,b);i++){
+            if(a%i==0 && b%i==0){
+                gcd=i;
+            }
+        }
+        return gcd;
+    }
+}
