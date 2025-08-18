d12f497811004b26635c72cde8e907eba09a4848
kashish1200 <84280963+kashish1200@users.noreply.github.com>
2025-08-19 00:33:43 +0530

    Update and rename Move Zeroes to Move Zeroes.java

--- LeetCode/Move Zeroes.java
+++ LeetCode/Move Zeroes.java
@@ -12,11 +12,31 @@ class Solution {
                 j++;
             }
         }
-        
     }
     public void swap(int nums[], int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+}
+
+
+//other approach
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int x =0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[x] = nums[i];
+                x++;
+            }
+            if(nums[i]==0) count++;
+        }
+        for(int i=x;i<n;i++){
+            nums[x] = 0;
+            x++;
+        }
     }
 }
