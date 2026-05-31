6b275566a4984962712893b64e3eb2491e19c3bf
kashish1200 <84280963+kashish1200@users.noreply.github.com>
2026-05-31 19:01:48 +0530

    Update Nearest Greater to Left.java

--- Aditya Verma/Stack/Nearest Greater to Left.java
+++ Aditya Verma/Stack/Nearest Greater to Left.java
@@ -1,4 +1,5 @@
 //Brute force approach - O(n)^2
+//Interview tip - For every element, I can look to its left and move backwards until I find a greater element. If I find one, that's the answer; otherwise it's -1. This would take O(n²) time in the worst case.
 
 class Solution {
     public ArrayList<Integer> preGreaterEle(int[] arr) {
@@ -21,6 +22,11 @@ class Solution {
     }
 }
 
+//Tip - The problem is that I'm repeatedly scanning the same elements. For example, when processing multiple elements, I may keep checking the same values on the left again and again.
+// For each element, I only care about previous elements that are larger than it. Smaller elements are not useful once a larger element appears after them
+
+// Imp - This suggests maintaining a stack of candidate Previous Greater Elements. While processing from left to right, I'll remove elements from the stack that are smaller than
+//     or equal to the current element because they can never serve as a Previous Greater Element anymore.
 
 //Optimized approach - Stack - O(n)
 
