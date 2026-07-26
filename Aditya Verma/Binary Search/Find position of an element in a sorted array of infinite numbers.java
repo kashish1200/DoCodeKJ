ddd9ec2239d1022e8a04a07e356f580e2509513f
kashish1200 <84280963+kashish1200@users.noreply.github.com>
2026-07-26 19:27:52 +0530

    Create Find position of an element in a sorted array of infinite numbers.java

---
+++ Aditya Verma/Binary Search/Find position of an element in a sorted array of infinite numbers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int search(int[] arr, int k) {
+        int start = 0;
+        int end = 1; //initially taking 1
+
+        //jab end element chhota hai target se tab tk--->
+        // Expand the search range, jab tk range me naa aa jaye target "k", tab tk end ko double krte raho...and start ko old end ki position
+        while (arr[end] < k) {
+            start = end;
+            end = end * 2;
+        }
+
+        // Binary search, jaise hi range m aa jaye toh normal binary search
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == k) {
+                return mid;
+            }
+            else if (arr[mid] < k) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
